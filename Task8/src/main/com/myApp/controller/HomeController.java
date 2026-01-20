7c614a2772e3d2ec496f9fc9e9c0da1f4365bbab
1Menna <meno903010@gmail.com>
2026-01-20 15:13:33 +0200

    Task8 Form Validation

---
+++ Task8/src/main/com/myApp/controller/HomeController.java
@@ -0,0 +1,45 @@
+package main.com.myApp.controller;
+
+import main.com.myApp.Models.UserModel;
+import main.com.myApp.dao.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import javax.validation.Valid;
+
+
+@Controller
+public class HomeController
+{
+    @Autowired
+    MyDatabase myDatabase;
+
+
+    @RequestMapping("/")
+    public String showHomePage(Model model)
+    {
+         model.addAttribute("userModel",new UserModel());
+        return "formPage";
+    }
+
+    @RequestMapping("/processForm")
+    public String processForm(@Valid @ModelAttribute("userModel") UserModel userModel, BindingResult bindingResult, Model model){
+
+        if(bindingResult.hasErrors())
+            return "formPage";
+
+
+        myDatabase.InsertIntoToDatabase(userModel);
+
+        model.addAttribute("userModelResult" , userModel);
+
+        return "confirmPage";
+    }
+}
+
+
---
+++ Task8/src/main/webapp/WEB-INF/view/confirmPage.jsp
@@ -0,0 +1,30 @@
+<html>
+<head>
+    <%--import Bootstrap--%>
+    <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/resources/css/bootstrap.min.css">
+    <%--import css file--%>
+    <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/resources/css/style.css">
+</head>
+<body >
+    <div class="text-center">
+        <h2 class="text-success">Data submitted successfully</h2>
+        <h3> Your Data <h3>
+        <table class="table">
+          <tr>
+            <th>userName</th>
+            <th>password</th>
+            <th>confirm password</th>
+            <th>Email</th>
+          </tr>
+          <tr>
+            <td>${userModelResult.userName}</td>
+            <td>${userModelResult.password}</td>
+            <td>${userModelResult.confirmPassword}</td>
+            <td>${userModelResult.email}</td>
+          </tr>
+        </table>
+    </div>
+</body>
+</html>
+
+
