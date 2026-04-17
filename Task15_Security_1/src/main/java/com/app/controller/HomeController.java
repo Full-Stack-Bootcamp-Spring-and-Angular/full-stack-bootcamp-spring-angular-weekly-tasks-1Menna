080e30a7587168dac3f7a07de4f187f1045c08cc
1Menna <meno903010@gmail.com>
2026-04-18 01:30:50 +0200

    security_1

---
+++ Task15_Security_1/src/main/java/com/app/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String homePage(){
+        return "home";
+    }
+
+    @RequestMapping("/admin")
+    public String adminPage(){
+        return "admin";
+    }
+
+    @RequestMapping("/system")
+    public String systemPage(){
+        return "system";
+    }
+
+}
