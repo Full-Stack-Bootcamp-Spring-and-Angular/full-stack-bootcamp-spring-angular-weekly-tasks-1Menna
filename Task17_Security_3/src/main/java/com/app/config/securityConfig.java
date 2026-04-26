14cedc10c4108e8c47cd263d7214b3e22b0d20a4
1Menna <meno903010@gmail.com>
2026-04-26 22:04:10 +0300

    security_3

---
+++ Task17_Security_3/src/main/java/com/app/config/securityConfig.java
@@ -0,0 +1,48 @@
+package com.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+@EnableWebSecurity
+public class securityConfig extends WebSecurityConfigurerAdapter {
+
+    // Authentication
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(users.username("menna").password("123").roles("ADMIN"))
+                .withUser(users.username("systemApp").password("456").roles("SYSTEM"));
+
+    }
+
+    // Authorization
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+         http.authorizeRequests()
+                 .antMatchers("/admin").hasRole("ADMIN")
+                 .antMatchers("/system").hasAnyRole("SYSTEM","ADMIN")
+                 .antMatchers("/resources/**").permitAll()
+                 .anyRequest().authenticated()
+                 .and()
+                 .formLogin()
+                 .loginPage("/login")
+                 .loginProcessingUrl("/authenticateTheUser")
+                 .permitAll()
+                 .and()
+                 .logout()
+                 .logoutUrl("/SignOut")
+                 .permitAll()
+                 .and()
+                 .exceptionHandling()
+                 .accessDeniedPage("/accessDenied");
+
+    }
+}
