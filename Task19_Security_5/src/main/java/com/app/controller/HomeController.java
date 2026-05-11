e825e87e7bf16d101aed120c86a5f8929851be11
1Menna <meno903010@gmail.com>
2026-05-11 23:09:01 +0300

    addNewUser Controller method that take form data and call service

--- Task19_Security_5/src/main/java/com/app/controller/HomeController.java
+++ Task19_Security_5/src/main/java/com/app/controller/HomeController.java
@@ -1,10 +1,17 @@
 package com.app.controller;
 
+import com.app.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    UserService userService;
+
 
     @RequestMapping("/")
     public String homePage(){
@@ -34,6 +41,11 @@ public class HomeController {
     @RequestMapping("/register")
     public String register(){
         return "register";
+    }
+
+    @RequestMapping("/addNewUser")
+    public void addNewUser(@RequestParam("username") String username, @RequestParam("password") String password){
+         userService.insertIntoSchema(username,password);
     }
 
 }
---
+++ Task19_Security_5/src/main/java/com/app/service/UserService.java
@@ -0,0 +1,13 @@
+package com.app.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    
+    public void insertIntoSchema(String username, String password){
+
+        
+    }
+    
+}
